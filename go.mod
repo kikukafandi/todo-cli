2df4b8ac906471d8c5c794270ad617949ae5b9a9
kikukafandi <jtierta@gmail.com>
2025-08-08 01:30:05 +0700

    feat init && cek file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kikukafandi/todo-cli
+
+go 1.22.0
---
+++ todo/task.go
@@ -0,0 +1,43 @@
+package todo
+
+import (
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	ID     int
+	name   string
+	status bool
+}
+
+func cekFileTodo() {
+	var fileName string = "todo.csv"
+	_, err := os.Stat(fileName)
+
+	if os.IsNotExist(err) {
+		fmt.Printf("File %s tidak ditemukan. apakah kamu ingin membuat file baru? (y/n)", fileName)
+		var response string
+		fmt.Scanln(response)
+
+		if response == "y" || response == "Y" {
+			file, err := os.Create(fileName)
+			if err != nil {
+				fmt.Println("Membuat file gagal:", err)
+				return
+			}
+			defer file.Close()
+			fmt.Println("File berhasil dibuat.")
+		} else {
+			fmt.Println("File tidak dibuat.")
+			return
+		}
+	} else {
+		fmt.Println("File ditemukan, lanjut proses")
+	}
+
+}
+
+func addTodo() {
+
+}
